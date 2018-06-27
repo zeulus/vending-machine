5cefb153304c38c7609c013a76fd4dd2fdbea61a
Marcin Wójcik <zeulus@gmail.com>
2018-06-27 23:09:40 +0200

    total value of inserted coins returned by machine

--- src/Aggregate/Machine.php
+++ src/Aggregate/Machine.php
@@ -6,13 +6,30 @@ use Zeulus\VendingMachine\Entity\Coin;
 
 class Machine
 {
+	/**
+	 * @var array Coin[]
+	 */
+	private $insertedCoins = [];
+
+	/**
+	 * @param Coin $coin
+	 */
     public function insertCoin(Coin $coin)
     {
-
+		$this->insertedCoins[] = $coin;
     }
 
+	/**
+	 * @return int
+	 */
     public function getCredits()
     {
-        // TODO: write logic here
+        return array_reduce(
+        	$this->insertedCoins,
+	        function ($sum, Coin $item) {
+        	    return $sum = $sum += $item->getValue();
+            },
+	        0
+        );
     }
 }
