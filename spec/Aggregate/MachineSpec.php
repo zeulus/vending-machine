91205e0a827758aee7a80210ff3d117c3b4cb400
Marcin Wójcik <zeulus@gmail.com>
2018-06-27 22:59:59 +0200

    failing tests for checking credits

--- spec/Aggregate/MachineSpec.php
+++ spec/Aggregate/MachineSpec.php
@@ -4,12 +4,33 @@ namespace spec\Zeulus\VendingMachine\Aggregate;
 
 use Zeulus\VendingMachine\Aggregate\Machine;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use Zeulus\VendingMachine\Entity\Coin;
 
 class MachineSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType(Machine::class);
+    }
+
+    function it_should_allow_you_to_insert_coins(Coin $coin)
+    {
+		$this->insertCoin($coin);
+    }
+
+    function it_should_reject_an_item_which_is_not_a_coin()
+    {
+    	$this->shouldThrow('\TypeError')->duringInsertCoin('aaaaa');
+    }
+
+    function it_should_return_total_value_of_inserted_coins()
+    {
+    	$sum = 0;
+    	for ($i=1; $i<3; $i++) {
+    		$this->insertCoin(new Coin($i));
+    		$sum += $i;
+	    }
+
+    	$this->getCredits()->shouldReturn($sum);
     }
 }
--- src/Aggregate/Machine.php
+++ src/Aggregate/Machine.php
@@ -2,6 +2,17 @@
 
 namespace Zeulus\VendingMachine\Aggregate;
 
+use Zeulus\VendingMachine\Entity\Coin;
+
 class Machine
 {
+    public function insertCoin(Coin $coin)
+    {
+
+    }
+
+    public function getCredits()
+    {
+        // TODO: write logic here
+    }
 }
